bfcab677a73dd7200cc1ad2ee11a5565cb57a52e
raphzam <60793109+raphzam@users.noreply.github.com>
2021-06-17 16:36:29 -0400

    Created JSON response for POST request to route /test

--- server.js
+++ server.js
@@ -3,6 +3,30 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello Lyft, my name is Raphael Jason Zamora.");
+});
+
+app.post("/test", (req, res) => {
+  function cutString(str) {
+    let retVal = "";
+    if (str.length < 3) {
+      retVal = "String is less than 3 characters";
+    } else {
+      for (let i = 2; i < str.length; i += 3) {
+        retVal += str[i];
+      }
+    }
+
+    let obj = {
+      return_string: retVal,
+    };
+
+    return obj;
+  }
+  res.json(cutString(req.body.string_to_cut));
+});
+
 app.listen(3000, () => {
   console.log("Server started...");
 });
